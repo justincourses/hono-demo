a713b3e8f59c263475803705d6d4c77d92909e89
Justin Chen <yokogaisha@gmail.com>
2025-09-04 14:36:51 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get("/health", (c) => {
+  return c.json({ status: "OK" });
+});
+
+app.get("/client", (c) => {
+  const request = c.req;
+  const userAgent = request.header("user-agent") || "Unknown";
+
+  return c.json({ browser: userAgent, method: request.method, url: request.url, ip: request.header("cf-connecting-ip") || "Unknown" });
+});
+
+export default app
